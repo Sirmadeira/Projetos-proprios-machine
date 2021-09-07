5756d1589048aa1f4f5854e47f8e392596b200ae
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-09-07 14:06:44 -0300

    a

--- Desafios legais/Numpy array zuado.py
+++ Desafios legais/Numpy array zuado.py
@@ -1,22 +1,41 @@
 import numpy as np
-
 np.set_printoptions(threshold=np.inf)
-a=np.array([[9,-4,1,0,0,0,0],
-			[-4,6,-4,1,0,0,0]])
-b=np.array([[0,0,0,1,-4,5,-2],
-			[0,0,0,0,1,-2,1]],)
-np_base=np.array([[1,-4,6,-4,1,0,0],
-				[0,1,-4,6,-4,1,0],
-				[0,0,1,-4,6,-4,1]])
 
-# print(a)
-# print(b)
-# print(np_base)
-c=np.tile(np_base,(196,1))
-d=np.append(c,b,axis=0)
-e=np.insert(d,0,a,axis=0)
+def a():
+    a=[]
 
-print(e)
+    # Inicializando a matriz com 0 em todos as posições
+    for i in range(200):
+        j=a.append(list())
+        for j in range(200):
+            a[i].append(0)
+
+    # Definindo numeros constantes
+    a[0][0] = 9
+    a[0][1] = -4
+    a[0][2] = -1
+    a[1][0] = -4
+    a[1][1] = 6
+    a[1][2] = -4
+    a[1][3] = -1
+    a[198][196] = 1
+    a[198][197] = 4
+    a[198][198] = -5
+    a[198][199] = -2
+    a[199][197] = 1
+    a[199][198] = -2
+    a[199][199] = 1
+
+    # Gerando padrão diagonal
+    for i in range(2, 198):
+        a[i][i-2] = 1
+        a[i][i-1] = -4
+        a[i][i] = 6
+        a[i][i+1] = -4
+        a[i][i+2] = 1
+    return np.array(a)
+
+print(a())
 
 
 
