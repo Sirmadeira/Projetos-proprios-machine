e275146c6adbb427cbf4f6c6098cc0e5ade030ae
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-09-20 14:21:36 -0300

    a

--- ProjetoXgboost/Xgboost.py
+++ ProjetoXgboost/Xgboost.py
@@ -17,6 +17,34 @@ from sklearn.metrics import accuracy_score
 #Realmente bugo aqui tive que usar absulute nao faco ideia pq
 df=pd.read_csv('C:/Users/FranciscoFroes/Documents/GitHub/Projetos-proprios-machine/ProjetoXgboost/parkinsons.csv')
 
+#print(df.head())
+
+features=df.loc[:,df.columns!='status'].values[:,1:]
+#print(features)
+#Features=Todas as colunas menos status
+labels=df.loc[:,'status'].values
+#Label= Status, se tem ou nao tem
+
+print(labels[labels==1].shape[0],labels[labels==0].shape[0])
+#Numero de status positivos numero de status negativos
+
+escalanador=MinMaxScaler((-1,1))
+#Isso daki vai definir o valor em que se encontra as features,basicamente
+#Dividimo e arrendomos para o valor mais proximo
+X=escalanador.fit_transform(features)
+#Transformando
+y=labels
+
+
+X_treino,X_teste,y_treino,y_teste=train_test_split(X,y,test_size=0.2,random_state=7,)
+#Random state e o quanto a gente vai ficar trocando randomicamente os dados
+
+modelo=XGBClassifier(use_label_encoder=False,eval_metric='mlogloss')
+modelo.fit(X_treino,y_treino)
+
+y_pred=modelo.predict(X_teste)
+print(accuracy_score(y_teste,y_pred)*100)
+
 
 
 
