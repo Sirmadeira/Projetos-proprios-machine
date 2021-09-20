c9bb312b9e937dd6aeb4cabd8c75a4b6243c853f
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-09-20 12:51:47 -0300

    a

---
+++ Projeto xgboost/Xgboost.py
@@ -0,0 +1,23 @@
+#O dataset tera como enfoco principal, identificar 
+#Os atributos que contribuem  para a doenca de parkinson, algumas dessa variaveis
+#Obtive, discutindo com a namorado do meu irmao que e medica
+#Agradeco Hannah pelas infos do que voces procuram e do que geralmente originam
+#O sofrimento, vlw flw
+#Se voce quiser os detalhes, do dataset so acessar a via parkinsons.names
+#La vai ter uma explicacao em ingles de cada variavel
+#Trabalhei por conda, entao nao tem como compartilhas o environment, sorry :<
+
+import numpy as np
+import pandas as pd
+import os, sys
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+df=pd.read_csv('Projeto_xgboost/parkinsons.data')
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Projetos machine
- Projetos de machine learning
+ Projetos de machine learning que fiz para tonificar minhas habilidades.
+ Os detalhes de cada um esta comentado dentro dos codigos montados.
+ Alguns dos dataset foram formulados por mim outros roubei do kaggle.
+ Kekerino!
